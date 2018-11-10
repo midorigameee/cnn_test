b676ab9aa30a77491b0e9684e0736b7dc217752f
shin-2D7 <xqbodp727@gmail.com>
2018-11-10 15:34:48 +0900

    CNN is also done.

--- train_model.py
+++ train_model.py
@@ -30,55 +30,60 @@ print("device:", device)
 NUM_EPOCHS = 50
 NUM_CLASSES = 4
 IMAGE_SIZE = 32
-HIDDEN_SIZE = 500
+HIDDEN_SIZE = 512
 BATCH_SIZE = 10
 LEARNING_RATE = 0.001
 
 
-# Convolutional neural network (two convolutional layers)
-class ConvNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super(ConvNet, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(7*7*32, num_classes)
+# CNN
+#   https://qiita.com/kazetof/items/6a72926b9f8cd44c218e
+# conv2dの引数は左から，インプットのチャンネルの数，アウトプットのチャンネルの数，カーネルサイズ
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 4)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        return out
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-
+# MLP
+#   http://aidiary.hatenablog.com/entry/20180204/1517705138
 class MultiLayerPerceptron(nn.Module):
-
     def __init__(self, image_size, hidden_size, num_classes):
         super(MultiLayerPerceptron, self).__init__()
         # ハイパーパラメータ
         self.image_size = image_size
-        self.input_size = image_size * image_size
+        self.input_size = image_size * image_size * 3  # RGB想定なので*3
         self.hidden_size = hidden_size
         self.num_classes = num_classes
 
         # ネットワーク構造
-        self.fc1 = nn.Linear(self.input_size, self.hidden_size)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(self.hidden_size, self.num_classes)
+        self.softmax = nn.LogSoftmax()
+        self.fc1 = nn.Linear(self.input_size, self.hidden_size)
+        self.fc2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc3 = nn.Linear(self.hidden_size, self.num_classes)
 
     def forward(self, x):
-        x = x.view(-1, self.image_size * self.image_size)
+        x = x.view(-1, self.image_size * self.image_size * 3)  # RGB想定なので*3
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.softmax(out)
         return out
 
 # dataset_dirは以下のような構造を持つディレクトリであること
@@ -94,7 +99,9 @@ class MultiLayerPerceptron(nn.Module):
 #       ...
 #   ...
 #
-def makeDataset(dataset_dir, color_type=cv2.IMREAD_GRAYSCALE):
+# 現在は全て画像サイズが同じRGB画像を想定したデータセット作成関数
+# グレースケールも無理やりRGB画像として読み込む仕様
+def makeDataset(dataset_dir):
     # 分類したいクラスをリスト化
     class_list = os.listdir(dataset_dir)
     class_list.sort()
@@ -118,27 +125,30 @@ def makeDataset(dataset_dir, color_type=cv2.IMREAD_GRAYSCALE):
         for image_name in image_list:
             # 画像の読み込み
             image_path = os.path.join(class_path, image_name)
-            image_data = cv2.imread(image_path, color_type)
+            image_data = cv2.imread(image_path, cv2.IMREAD_COLOR)
+
+            # (縦, 横, チャンネル)を(チャンネル, 縦, 横)に変換
+            # CNNで学習させる場合はこちらの方が使いやすいため
+            # 通常のMLPの際は1次元にするためどちらでも問題ない
+            image_data = np.transpose(image_data, (2, 0, 1))
 
             # 画像をリストに追加
-            data_x.append([image_data])
+            data_x.append(image_data)
 
-            # 教師データを作成し、リストに追加
+            # one-hot表現の教師データを作成し、リストに追加
             supervisor = np.zeros(class_num)
             supervisor[class_idx] = 1
-            # supervisor = class_idx
             data_y.append(supervisor)
-
 
     # データセットをlistからnumpyに変換
     data_x = np.array(data_x, dtype="float32")
     data_y = np.array(data_y, dtype="int32")
 
     # 訓練用とテスト用に分割
-    x_train, x_test, y_train, y_test = train_test_split(
+    train_x, test_x, train_y, test_y = train_test_split(
             data_x, data_y, test_size=1/5, random_state=0)
 
-    return x_train, x_test, y_train, y_test, label
+    return train_x, test_x, train_y, test_y, label
 
 
 # データセットを作成しTensor化
@@ -156,7 +166,8 @@ test = torch.utils.data.TensorDataset(test_x, test_y)
 test_loader = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE, shuffle=False)
 
 # モデル、損失関数、最適化関数の定義
-model = MultiLayerPerceptron(IMAGE_SIZE, HIDDEN_SIZE, NUM_CLASSES).to(device)
+# model = MultiLayerPerceptron(IMAGE_SIZE, HIDDEN_SIZE, NUM_CLASSES).to(device)
+model = CNN().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
@@ -180,21 +191,22 @@ for epoch in range(NUM_EPOCHS):
             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                    .format(epoch+1, NUM_EPOCHS, i+1, total_step, loss.item()))
 
+
 # Test the model
-model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
-with torch.no_grad():
+model.eval()  # ネットワークを推論モードに切り替える
+with torch.no_grad():   # 推論中は勾配の保存を止める（メモリのリーク？を防ぐため）
     correct = 0
     total = 0
     for images, labels in test_loader:
         images = images.to(device)
         labels = labels.to(device)
+
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
-        correct += (predicted == torch.max(labels, 1)[1]).sum().item() # default labels
+        correct += (predicted == torch.max(labels, 1)[1]).sum().item()
 
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
-
+    print('Test Accuracy: {} %' .format(100 * correct / total))
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
